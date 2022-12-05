25a83dfe64194c0e76d325b167eab5603be72139
= <=>
2022-12-05 20:54:05 +0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Shoot Ace`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
